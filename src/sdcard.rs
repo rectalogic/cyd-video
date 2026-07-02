d15a4b6a52ca4d1977b90563a4e8d230d89e1905
Andrew Wason <rectalogic@rectalogic.com>
2026-07-02 21:53:11 +0000

    working SD card and video playback on both chips. But stuck at 400khz.

--- src/sdcard.rs
+++ src/sdcard.rs
@@ -1,7 +1,7 @@
 use crate::error::Error;
 use core::{convert::Infallible, ops::AsyncFnOnce};
 use embassy_time::Delay;
-use embedded_hal::spi::SpiBus;
+use embedded_hal::{delay::DelayNs};
 use embedded_hal_bus::spi::ExclusiveDevice;
 use embedded_sdmmc::{
     SdCardError, TimeSource, Timestamp, VolumeIdx, VolumeManager, filesystem::ToShortFileName,
@@ -13,7 +13,10 @@ use esp_hal::peripherals::{GPIO38, GPIO39, GPIO40, GPIO47, SPI3};
 use esp_hal::{
     Blocking,
     gpio::{Level, Output, OutputConfig},
-    spi::master::{Config as SpiConfig, Spi},
+    spi::{
+        Mode as SpiMode,
+        master::{Config as SpiConfig, Spi},
+    },
     time::Rate,
 };
 
@@ -47,31 +50,25 @@ pub struct SdCard {
 
 impl SdCard {
     pub fn new(peripherals: Peripherals) -> Result<Self, Error<Infallible, Infallible>> {
-        let mut spi = Spi::new(
+        let spi = Spi::new(
             peripherals.spi3,
-            SpiConfig::default().with_frequency(Rate::from_khz(400)), // <=400kHz required for initialization
+            SpiConfig::default()
+                .with_frequency(Rate::from_khz(400)) // <=400kHz required for initialization
+                .with_mode(SpiMode::_0),
         )?
         .with_sck(peripherals.sclk)
         .with_mosi(peripherals.mosi)
         .with_miso(peripherals.miso);
 
-        // Send 74+ clock cycles (10 bytes = 80 cycles)
-        // CS doesn't need to exist yet - it just needs to NOT be asserted
-        let mut dummy = [0xFF; 10];
-        SpiBus::transfer_in_place(&mut spi, &mut dummy)?;
+        let mut cs = Output::new(peripherals.cs, Level::High, OutputConfig::default());
+        cs.set_high();
+        Delay.delay_ms(10);
 
-        let cs = Output::new(peripherals.cs, Level::High, OutputConfig::default());
         let spi_dev = ExclusiveDevice::new(spi, cs, Delay).unwrap();
         let sdcard = embedded_sdmmc::SdCard::new(spi_dev, Delay);
 
         // Force initialization
         let _ = sdcard.num_bytes();
-
-        // Reconfigure frequency
-        sdcard.spi(|spi| {
-            spi.bus_mut()
-                .apply_config(&SpiConfig::default().with_frequency(Rate::from_mhz(80)))
-        })?;
 
         let volume_manager = VolumeManager::new(sdcard, DummyTimesource);
 
