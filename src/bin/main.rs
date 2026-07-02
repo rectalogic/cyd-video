09b2643faf713e742f5381a7dc6382bad7a953bb
Andrew Wason <rectalogic@rectalogic.com>
2026-07-02 20:54:42 +0000

    fix esp32s3 memory issue

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -33,7 +33,10 @@ async fn main(_spawner: Spawner) -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
+    #[cfg(esp32)]
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+    #[cfg(esp32s3)]
+    esp_alloc::heap_allocator!(size: 98768);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     let sw_interrupt =
