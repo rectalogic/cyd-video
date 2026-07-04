5c42aa113efc76b0cae73a6cd19d4a9bc7caf493
Andrew Wason <rectalogic@rectalogic.com>
2026-07-04 20:45:29 +0000

    force yuv420p encoding

--- scripts/encode.sh
+++ scripts/encode.sh
@@ -25,7 +25,7 @@ shift $(( OPTIND - 1 ))
 # Scale to fit in 320x240, maintaining AR, and then pad width/height to multiple of 8
 ffmpeg -hide_banner -i "$1" \
   -an \
-  -vf "framerate=${fps},scale=320x240:force_original_aspect_ratio=decrease:reset_sar=1:flags=lanczos,pad=ceil(iw/8)*8:ceil(ih/8)*8:(ow-iw)/2:(oh-ih)/2" \
+  -vf "framerate=${fps},scale=320x240:force_original_aspect_ratio=decrease:reset_sar=1:flags=lanczos,pad=ceil(iw/8)*8:ceil(ih/8)*8:(ow-iw)/2:(oh-ih)/2,format=yuv420p" \
   -c:v mjpeg -q:v 10 \
   -f avi \
   -y "$2"
